b1cfdd04f76f0930546f7a6e3dd20f94358262c5
Tom Emem <tomemem361@gmail.com>
2026-04-13 12:21:09 +0100

    Complete Week 6 BST assignment: implemented Insert unique check, Contains, TraverseBackward, GetHeight, and InsertMiddle for balanced tree creation

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -10,36 +10,61 @@ public class Node
     }
 
     public void Insert(int value)
-    {
-        // TODO Start Problem 1
+{
+    // Prevent duplicates
+    if (value == Data)
+        return;
 
-        if (value < Data)
-        {
-            // Insert to the left
-            if (Left is null)
-                Left = new Node(value);
-            else
-                Left.Insert(value);
-        }
+    if (value < Data)
+    {
+        // Insert to the left
+        if (Left is null)
+            Left = new Node(value);
         else
-        {
-            // Insert to the right
-            if (Right is null)
-                Right = new Node(value);
-            else
-                Right.Insert(value);
-        }
+            Left.Insert(value);
     }
+    else
+    {
+        // Insert to the right
+        if (Right is null)
+            Right = new Node(value);
+        else
+            Right.Insert(value);
+    }
+}
 
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        if (value == Data)
+            return true;
+
+        if (value < Data)
+        {
+            if (Left is null)
+                return false;
+
+            return Left.Contains(value);
+        }
+        else
+        {
+            if (Right is null)
+                return false;
+
+            return Right.Contains(value);
+        }
     }
 
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = 0;
+        int rightHeight = 0;
+
+        if (Left is not null)
+            leftHeight = Left.GetHeight();
+
+        if (Right is not null)
+            rightHeight = Right.GetHeight();
+
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
--- week06/code/Trees.cs
+++ week06/code/Trees.cs
@@ -48,6 +48,20 @@ public static class Trees
     /// <param name="bst">the BinarySearchTree in which to insert the values</param>
     private static void InsertMiddle(int[] sortedNumbers, int first, int last, BinarySearchTree bst)
     {
-        // TODO Start Problem 5
+        // Base case
+        if (first > last)
+            return;
+
+        // Find the middle element
+        int mid = (first + last) / 2;
+
+        // Insert the middle value into the tree
+        bst.Insert(sortedNumbers[mid]);
+
+        // Recursively process the left half
+        InsertMiddle(sortedNumbers, first, mid - 1, bst);
+
+        // Recursively process the right half
+        InsertMiddle(sortedNumbers, mid + 1, last, bst);
     }
 }
